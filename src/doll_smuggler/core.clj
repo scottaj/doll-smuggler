52e0a695795d2097ec0a81b60318e267f5e194dd
Al Scott <al.scott@atomicobject.com>
2012-05-15 09:21:58 -0400

    Initial commit.
    Used Leiningen to generate project skeleton.

---
+++ src/doll_smuggler/core.clj
@@ -0,0 +1,1 @@
+(ns doll-smuggler.core)
---
+++ test/doll_smuggler/test/core.clj
@@ -0,0 +1,6 @@
+(ns doll-smuggler.test.core
+  (:use [doll-smuggler.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
