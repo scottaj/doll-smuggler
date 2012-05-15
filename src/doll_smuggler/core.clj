ebf2013b62071f1991292120e6dda75fdfb7e0b8
Al Scott <al.scott@atomicobject.com>
2012-05-15 14:02:14 -0400

    Wrote function to read in data file.
    This function still has a known error where it will not read in
    the last entry in the data file.

--- src/doll_smuggler/core.clj
+++ src/doll_smuggler/core.clj
@@ -1,1 +1,28 @@
-(ns doll-smuggler.core)
+(ns doll-smuggler.core
+    :use [clojure.string])
+
+
+
+
+(defn main
+  
+  )
+
+
+
+(defn load-dolls
+  [file-name]
+  (let [doll-data (split-lines (slurp file-name))]
+       (loop [i 0 payload {}]
+             (if (> i (count doll-data))
+                payload
+                (let [line (split (nth doll-data i) #",")]
+                      (recur (inc i) 
+                              (conj payload [(first line) 
+                                            [(Integer/parseInt (second line)) 
+                                              (Integer/parseInt (last line))]])))))))
+
+
+(defn pack-dolls
+  [dolls weight-limit])
+
