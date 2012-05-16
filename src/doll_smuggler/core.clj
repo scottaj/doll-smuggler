b1c2b9ff63db8b5e2eb01e88c766e3ae7a79dc43
Al Scott <al.scott@atomicobject.com>
2012-05-16 15:35:47 -0400

    Finished cases for solution matrix, need to test them.

--- src/doll_smuggler/core.clj
+++ src/doll_smuggler/core.clj
@@ -44,12 +44,19 @@
   
   (loop [solutions [] row [] i -1 w 1]
         (if (= i (count dolls))
-            keep
+            solutions
             (let [doll-i (nth dolls i)]
                  (when (<= w weight-limit)
                        (recur (conj solutions row) [] (inc i) 1))
                  (when (< i 0)
-                       (recur [] (conj row [0 0]) i (inc w)))
-                 (if (< w (:weight doll-i))
-                     (recur solutions (conj [(first (nth (nth solutions i) w)) 0]) i (inc w))
-                     )))))
+                       (recur solutions (conj row [0 0]) i (inc w)))
+                 (when (< w (:weight doll-i))
+                       (recur solutions (conj [(first (nth (nth solutions i) w)) 0]) i (inc w)))
+                 (when (= w (:weight doll-i))
+                     (if (> (:value doll-i) (first (nth (nth solutions i) w)))
+                         (recur solutions (conj row [(:value doll-i) 1] i (inc w)))
+                         (recur solutions (conj row [(first (nth (nth solutions i) w)) 0] i (inc w)))))
+                 (let [cv (+ (:value doll-i) (first (nth (nth solutions i) (- w (:value doll-i)))))]
+                      (if (> (first (nth (nth solutions i) w)) cv)
+                          (recur solutions (conj row [(first (nth (nth solutions i) w)) 0]) i (inc w))
+                          (recur solutions (conj row [cv 1]) i (inc w))))))))
