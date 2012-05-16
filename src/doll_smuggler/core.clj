1e4a332413765c5760fabd282ebc17c73c7d205c
Al Scott <al.scott@atomicobject.com>
2012-05-16 16:45:48 -0400

    Reordered functions in core file.

--- src/doll_smuggler/core.clj
+++ src/doll_smuggler/core.clj
@@ -1,12 +1,8 @@
 (ns doll-smuggler.core
-    :use [clojure.string])
+    :use [clojure.string :only [split split-lines]])
 
 
 (defrecord Doll [id weight value])
-
-(defn main
-  [file-name weight-limit]
-  )
 
 
 
@@ -26,13 +22,45 @@
                             (inc i)))))))
 
 
+
+(defn calculate-solution-matrix
+  "Calculates the matrix for the optimal solution."
+  [dolls weight-limit]
+  
+  (loop [solutions [] row [] i -1 w 1]
+        (if (= i (count dolls))
+            solutions
+            (let [doll-i (nth dolls i)]
+                 (when (<= w weight-limit)
+                       (recur (conj solutions row) [] (inc i) 1))
+                 
+                 (when (< i 0)
+                       (recur solutions (conj row [0 0]) i (inc w)))
+                 
+                 (when (< w (:weight doll-i))
+                       (recur solutions (conj [(first (nth (nth solutions i) (- w 1))) 0]) i (inc w)))
+                 
+                 (when (= w (:weight doll-i))
+                     (if (> (:value doll-i) (first (nth (nth solutions i) (- w 1))))
+                         (recur solutions (conj row [(:value doll-i) 1] i (inc w)))
+                         (recur solutions (conj row [(first (nth (nth solutions i) (- w 1))) 0] i (inc w)))))
+                 
+                 (let [cv (+ (:value doll-i) (first (nth (nth solutions i) (- (- w 1) (:value doll-i)))))]
+                      (if (> (first (nth (nth solutions i) w)) cv)
+                          (recur solutions (conj row [(first (nth (nth solutions i) (- w 1))) 0]) i (inc w))
+                          (recur solutions (conj row [cv 1]) i (inc w))))))))
+
+
+
 (defn pack-dolls
   "Selects the optimal subset of dolls from the given set with the given weight constraint."
   [dolls weight-limit]
   
   (cond
-    (= (count dolls 0)) []
+    (= (count dolls) 0) []
+    
     (= weight-limit 0) []
+    
     true (let [solutions (calculate-solution-matrix dolls weight-limit)]
               (loop [handbag [] w (- weight-limit 1) i (- (count solutions) 1)]
                     (if (or (= i 0) (= w 0))
@@ -42,26 +70,7 @@
                             (recur handbag w (dec i))))))))
 
 
-
-(defn calculate-solution-matrix
-  "Calculates the keep matrix for the optimal solution."
-  [dolls weight-limit]
+(defn main
+  [file-name weight-limit]
   
-  (loop [solutions [] row [] i -1 w 1]
-        (if (= i (count dolls))
-            solutions
-            (let [doll-i (nth dolls i)]
-                 (when (<= w weight-limit)
-                       (recur (conj solutions row) [] (inc i) 1))
-                 (when (< i 0)
-                       (recur solutions (conj row [0 0]) i (inc w)))
-                 (when (< w (:weight doll-i))
-                       (recur solutions (conj [(first (nth (nth solutions i) (- w 1))) 0]) i (inc w)))
-                 (when (= w (:weight doll-i))
-                     (if (> (:value doll-i) (first (nth (nth solutions i) (- w 1))))
-                         (recur solutions (conj row [(:value doll-i) 1] i (inc w)))
-                         (recur solutions (conj row [(first (nth (nth solutions i) (- w 1))) 0] i (inc w)))))
-                 (let [cv (+ (:value doll-i) (first (nth (nth solutions i) (- (- w 1) (:value doll-i)))))]
-                      (if (> (first (nth (nth solutions i) w)) cv)
-                          (recur solutions (conj row [(first (nth (nth solutions i) (- w 1))) 0]) i (inc w))
-                          (recur solutions (conj row [cv 1]) i (inc w))))))))
+  (pack-dolls (load-dolls file-name) weight-limit))
